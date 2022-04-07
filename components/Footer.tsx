2a6b1825ea450236a634b3d85bbfc09a229dbf2a
Ernest Irabor <irabeny89@gmail.com>
2022-04-07 20:04:45 +0100

    💄 add footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Footer() {
+  return (
+    <footer className="text-center mt-5">
+      Powered by{" "}
+      <span className={styles.logo}>
+        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+      </span>
+      <div>Made by Ernest</div>
+    </footer>
+  );
+}
