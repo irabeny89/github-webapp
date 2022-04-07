f87eb813e043a04896cff55263c63b6c2d6811aa
Ernest Irabor <irabeny89@gmail.com>
2022-04-07 20:59:20 +0100

    remove unused modules

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
 import Container from "react-bootstrap/Container";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import Button from "react-bootstrap/Button";
-import Spinner from "react-bootstrap/Spinner";
-import Table from "react-bootstrap/Table";
-import { useEffect, useState } from "react";
-import github from "../axios";
 import SearchBox from "../components/SearchBox";
 import ResultTable from "../components/ResultTable";
 import Footer from "../components/Footer";
