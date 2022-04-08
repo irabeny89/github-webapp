f8803726c6de8ae9b5a82b8daaccaaa897d28fb6
Ernest Irabor <irabeny89@gmail.com>
2022-04-08 11:56:28 +0100

    refactor to suit context

--- components/ResultTable.tsx
+++ components/ResultTable.tsx
@@ -2,8 +2,58 @@ import Table from "react-bootstrap/Table";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Spinner from "react-bootstrap/Spinner";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import { useContext, useRef, useState } from "react";
+import github from "../axios";
+import { generateUri } from "@/utils/index";
+import SearchContext from "@/contexts/SearchContext";
 
 export default function ResultTable() {
+  const [{ searchData, searchTerm }, dispatch] =
+      useContext(SearchContext),
+    [isLoadingPrev, setLoadingPrev] = useState(false),
+    [isLoadingNext, setLoadingNext] = useState(false),
+    pageNumber = useRef(1);
+
+  const handlePreviousPage = async () => {
+    pageNumber.current > 1 && setLoadingPrev(true),
+      github(generateUri(searchTerm, pageNumber.current - 1))
+        .then(({ data }: any) => (setLoadingPrev(false), dispatch({
+          type: "setSearchData",
+          value: data
+        })))
+        .catch(
+          (error: any) => (
+            console.error(error),
+            dispatch({
+              type: "setErrorMessage",
+              value: error.message
+            }),
+            setLoadingPrev(false)
+          )
+        );
+  };
+  const handleNextPage = async () => {
+    setLoadingNext(true),
+      github(generateUri(searchTerm, pageNumber.current + 1))
+        .then(({ data }: any) => (setLoadingNext(false), dispatch({
+          type: "setSearchData",
+          value: data
+        })))
+        .catch(
+          (error: any) => (
+            console.error(error),
+            dispatch({
+              type: "setErrorMessage",
+              value: error.message
+            }),
+            setLoadingNext(false)
+          )
+        );
+  };
+
   return (
     <Container>
       <Row className="my-5">
@@ -11,17 +61,57 @@ export default function ResultTable() {
           <Table striped bordered hover size="sm" responsive>
             <thead>
               <tr>
-                <th>Repo</th>
-                <th>Author</th>
+                <th>#</th>
+                <th>Repo Name</th>
+                <th>Author Name</th>
                 <th>Stars</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td></td>
-              </tr>
+              {searchData?.items?.map(
+                (
+                  {
+                    id,
+                    owner: { login, html_url: _html_url },
+                    stargazers_count,
+                    name,
+                    html_url,
+                  },
+                  i
+                ) => (
+                  <tr key={id}>
+                    <td>{++i}</td>
+                    <td>
+                      <a href={html_url}>{name}</a>
+                    </td>
+                    <td>
+                      <a href={_html_url}>{login}</a>
+                    </td>
+                    <td>{stargazers_count}</td>
+                  </tr>
+                )
+              )}
             </tbody>
           </Table>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <ButtonGroup>
+            <Button
+              className="border-3 border-white"
+              onClick={handlePreviousPage}
+            >
+              {isLoadingPrev ? <Spinner animation="grow" /> : "\u003c"}
+            </Button>
+            <Button
+              className="border-3 border-dark"
+              variant="outline-secondary"
+              onClick={handleNextPage}
+            >
+              {isLoadingNext ? <Spinner animation="grow" /> : "\u003e"}
+            </Button>
+          </ButtonGroup>
         </Col>
       </Row>
     </Container>
--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -4,22 +4,70 @@ import Row from "react-bootstrap/Row";
 import InputGroup from "react-bootstrap/InputGroup";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Spinner from "react-bootstrap/Spinner";
+import { FormEvent, useContext, useState } from "react";
+import github from "../axios";
+import { generateUri } from "@/utils/index";
+import SearchContext from "@/contexts/SearchContext";
 
 export default function SearchBox() {
+  const [isLoading, setLoading] = useState(false),
+    [, dispatch] = useContext(SearchContext),
+    handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      const searchTerm = new FormData(e.currentTarget).get("search") as string;
+      // if input is empty do not send network request
+      // then update the data states
+      !!searchTerm
+        ? (setLoading(true),
+          github(generateUri(searchTerm))
+            .then(
+              ({ data }: any) => (
+                setLoading(false),
+                dispatch({
+                  type: "setSearchData",
+                  value: data
+                }),
+                dispatch({
+                  type: "setSearchTerm",
+                  value: searchTerm
+                })
+              )
+            )
+            .catch(
+              (error: any) => (
+                console.error(error),
+                dispatch({
+                  type: "setErrorMessage",
+                  value: error.message
+                }),
+                setLoading(false)
+              )
+            ))
+        : e.preventDefault(),
+        e.stopPropagation();
+    };
+
   return (
     <Container fluid>
       <Row className="justify-content-center">
         <Col sm="10">
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <InputGroup className="justify-content-center my-3">
               <Form.FloatingLabel label="Enter search term">
                 <Form.Control
                   placeholder="Enter your search term"
                   arial-label="search GitHub repositories"
                   name="search"
+                  onChange={() => dispatch({
+                    type: "setSearchTerm",
+                    value: ""
+                  })}
                 />
               </Form.FloatingLabel>
-              <Button>Search</Button>
+              <Button type="submit">
+                {isLoading ? <Spinner animation="grow" /> : `Search`}
+              </Button>
             </InputGroup>
           </Form>
         </Col>
