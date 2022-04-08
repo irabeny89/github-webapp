71fe21b042cbb55d8bfeafdef5627f46ad0f3489
Ernest Irabor <irabeny89@gmail.com>
2022-04-08 11:51:16 +0100

    add toast component

---
+++ components/ToastAlert.tsx
@@ -0,0 +1,26 @@
+import ToastContainer from "react-bootstrap/ToastContainer";
+import Toast from "react-bootstrap/Toast";
+import { useContext } from "react";
+import SearchContext from "@/contexts/SearchContext";
+
+export default function ToastAlert() {
+  const [{ errorMessage }, dispatch] = useContext(SearchContext)
+  return (
+    <ToastContainer position="bottom-center">
+      <Toast
+        bg="danger"
+        autohide
+        show={!!errorMessage}
+        onClose={() => dispatch({
+          type: "setErrorMessage",
+          value: ""
+        })}
+      >
+        <Toast.Header className="justify-content-between h5">
+          Oops...
+        </Toast.Header>
+        <Toast.Body className="text-white">{errorMessage}</Toast.Body>
+      </Toast>
+    </ToastContainer>
+  );
+}
